39578012eb7365cc3c313c7e392f81d8bed7aa1c
Chaimae-Chakir <chaimaechakir24@gmail.com>
2023-04-02 13:52:02 +0200

    add Practical Activity 4 Code

--- practical activity4/src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
+++ practical activity4/src/main/java/ma/enset/patientsmvc/PatientsMvcApplication.java
@@ -12,18 +12,17 @@ import java.util.Date;
 @SpringBootApplication
 public class PatientsMvcApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(PatientsMvcApplication.class, args);
     }
-    @Bean
+    //@Bean
     CommandLineRunner commandLineRunner(PatientRepository patientRepository){
-        return args ->{
-            patientRepository.save(new Patient(null,"Hassan",new Date(),false,12));
-            patientRepository.save(new Patient(null,"Mohammed",new Date(),true,312));
-            patientRepository.save(new Patient(null,"Yasmine",new Date(),true,65));
-            patientRepository.save(new Patient(null,"Hanae",new Date(),false,32));
-            patientRepository.findAll().forEach(patient -> {
-                System.out.println(patient.getNom());
+        return args->{
+            patientRepository.save(new Patient(null,"Hassan",new Date(),false,122));
+            patientRepository.save(new Patient(null,"Mohammed",new Date(),true,321));
+            patientRepository.save(new Patient(null,"Yasmine",new Date(),true,165));
+            patientRepository.save(new Patient(null,"Hanae",new Date(),false,132));
+            patientRepository.findAll().forEach(p->{
+                System.out.println(p.getNom());
             });
         };
     }
